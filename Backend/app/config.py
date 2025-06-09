8a84db18100f12c705148fcf60c45a477a872428
alejandro <agallego@bts.io>
2025-06-09 18:53:52 +0200

    moddified deployment

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+BASE_DIR = Path(__file__).resolve().parent
 
-TEMP_FILES_DIR = os.path.join(BASE_DIR, r'data\temp_files')
-UPLOAD_FOLDER = os.path.join(BASE_DIR, r'data\uploads')
-TEMP_CLIPS_DIR = os.path.join(BASE_DIR, r'data\temp_clips')
-DATABASE_URL = "postgresql+psycopg2://admin:admin@localhost:5432/ClipExtract"
+TEMP_FILES_DIR = BASE_DIR / 'data' / 'temp_files'
+UPLOAD_FOLDER = BASE_DIR / 'data' / 'uploads'
+TEMP_CLIPS_DIR = BASE_DIR / 'data' / 'temp_clips'
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://admin:admin@localhost:5432/ClipExtract")
 
 def configure_app(app):
     for path in [TEMP_FILES_DIR, UPLOAD_FOLDER, TEMP_CLIPS_DIR]:
